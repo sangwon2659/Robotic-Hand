15192b16a9427710a2f8fddfd667e12650762766
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2020-09-02 14:12:54 +0900

    Update Sensor_read_teleop.py

--- ROS/Sensor_read_teleop.py
+++ ROS/Sensor_read_teleop.py
@@ -8,19 +8,25 @@ from msgpkg.msg import magnetic_encoder
 from msgpkg.msg import tactile
 from msgpkg.msg import msgtor
 
+# 6 channels including one encoder as the last slot
 n_ch = 6
+# Declaring the data array
 data = np.array((0,0,0,0,0,0))
 value = data
 alpha = 0.0
 torque = 0
+# Number of iteration until encoder offset determined
 iteration = 0
 torque_sum = 0
+# Offset for measurements of encoder
 encoder_baseline = 0
 
+# Port and baudrate declaration
 ser = serial.Serial(port = '/dev/ttyUSB4',baudrate=115200)
 
 def thread_run():
 	global value, pub, pub2, torque
+	# Publishing and posting the value array(5 tact info and 1 uncalibrated angle info) and the torque
 	pub.publish(value)
         pub2.publish(torque)
 	rospy.loginfo(value)
@@ -30,20 +36,29 @@ def thread_run():
 
 def talker():
 	global value, pub, pub2, torque, iteration, torque_sum, encoder_baseline
-
+	# Declaring publish variables
+	# Msg <tactile> for the value array
+	# Msg <msgtor> for the torque
 	pub = rospy.Publisher('tact', tactile, queue_size=1)
         pub2 = rospy.Publisher('torque', msgtor, queue_size = 1)
+	# Initializing node
 	rospy.init_node('talker', anonymous=True)
 
 	ser.reset_input_buffer()
 	thread_run()
 	
 	while not rospy.is_shutdown():
+		# Reading data from port
 		response = ser.readline()
 		if response.__len__() == n_ch*2+1:
+			# Putting in the 6 values into data
+			# Last byte not taken
 			data = np.array(struct.unpack('<HHHHHH', response[:-1]))
 			value = (alpha*value+(1-alpha)*data).astype('int32')
 			iteration += 1
+		# Finding the offset for the encoder
+		# Displayed as 'N/A' before the number of iterations is met
+		# Calibrates the angle info using the pre-done calibration and the current offset
 	        if iteration <= 500:
 	        	torque_sum += value[-1]
 			torque = 'N/A'
