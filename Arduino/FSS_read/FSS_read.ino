a17bab7daec1a9ccbf1a080d7441c47f4a01ac2b
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2020-09-03 14:02:40 +0900

    Update FSS_read.ino

--- Arduino/FSS_read/FSS_read.ino
+++ Arduino/FSS_read/FSS_read.ino
@@ -1,24 +1,36 @@
-#include "HX711.h" //HX711로드셀 엠프 관련함수 호출
-#define CLK  2  //엠프 클락 핀 넘버
+// Calling the header file
+#include "HX711.h" 
+// SCK Number
+#define CLK  2
+// 4 FSS Sensors
 #define n_channel 4
-int DOUT[n_channel] = {3,4,5,6}; //엠프 데이터 아웃 핀 넘버 선언
+int DOUT[n_channel] = {3,4,5,6};
+// Number of iterations for averaging
 int data_stack = 1;
+// Number of iterations for computing the offset
 int offset_stack = 50;
 long offset[n_channel];
 long value[n_channel];
+// Giving initial variables to the cpp file
+// Name set as FSS
 HX711 FSS(n_channel, DOUT, CLK); //엠프 핀 선언 
 
 void setup() {
-  Serial.begin(115200);  // 시리얼 통신 개방 
+  Serial.begin(115200);
+  // Putting in values for each slot in the offset array 
   for(int i=0; i<n_channel; i++){
+    // FSS.tare gives out the offset for DOUT[x]
     offset[i] = FSS.tare(DOUT[i], offset_stack);
   }
 }
 
 void loop() {
+  // Putting in values for each slot in the value array
   for(int i=0; i<n_channel; i++){
+    // FSS.get_value gives out the value of the sensor for DOUT[x]
     value[i] = FSS.get_value(DOUT[i], data_stack, offset[i]);
   }
+  // Printing or Plotting the values of the sensor
   for(int i=0; i<(n_channel-1);i++){
     Serial.print(value[i]);
     Serial.print("\t");
