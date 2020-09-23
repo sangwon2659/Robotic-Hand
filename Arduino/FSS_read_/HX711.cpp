24de44fa065edcd65d0ff13762117249df23b877
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2020-09-23 10:35:19 +0900

    Update HX711.cpp

--- Arduino/FSS_read_/HX711.cpp
+++ Arduino/FSS_read_/HX711.cpp
@@ -2,13 +2,12 @@
 #include "HX711.h"
 
 #if ARDUINO_VERSION <= 106
-    // "yield" is not implemented as noop in older Arduino Core releases, so let's define it.
-    // See also: https://stackoverflow.com/questions/34497758/what-is-the-secret-of-the-arduino-yieldfunction/34498165#34498165
-    void yield(void) {};
+void yield(void) {};
 #endif
 
+// Declaration of 객체 and initiating the begin function
 HX711::HX711(byte dout, byte pd_sck, byte gain) {
-  begin(dout, pd_sck, gain);
+	begin(dout, pd_sck, gain);
 }
 
 HX711::HX711() {
@@ -17,118 +16,123 @@ HX711::HX711() {
 HX711::~HX711() {
 }
 
+// Setting pinmodes for each 객체
 void HX711::begin(byte dout, byte pd_sck, byte gain) {
-  PD_SCK = pd_sck;
-  DOUT = dout;
+	PD_SCK = pd_sck;
+	DOUT = dout;
 
-  pinMode(PD_SCK, OUTPUT);
-  pinMode(DOUT, INPUT);
+	pinMode(PD_SCK, OUTPUT);
+	pinMode(DOUT, INPUT);
 
-  set_gain(gain);
+	set_gain(gain);
 }
 
+// Original algorithm of identifying whether the sensor is read to send data
+// When the sensor outputs low after sending a packet of data means it is ready to send another
 bool HX711::is_ready() {
-  return digitalRead(DOUT) == LOW;
+  delayMicroseconds(100);
+	return digitalRead(DOUT) == LOW;
 }
 
+// Gain is about how many additional clocks(MOSI-like) to be sent after receiving 3 bytes of data
 void HX711::set_gain(byte gain) {
-  switch (gain) {
-    case 128:   // channel A, gain factor 128
-      GAIN = 1;
-      break;
-    case 64:    // channel A, gain factor 64
-      GAIN = 3;
-      break;
-    case 32:    // channel B, gain factor 32
-      GAIN = 2;
-      break;
-  }
+	switch (gain) {
+	case 128:   
+		GAIN = 1;
+		break;
+	case 64:    
+		GAIN = 3;
+		break;
+	case 32:    
+		GAIN = 2;
+		break;
+	}
 
-  digitalWrite(PD_SCK, LOW);
-  read();
+	digitalWrite(PD_SCK, LOW);
+	read();
 }
+
 
 long HX711::read() {
-  // wait for the chip to become ready
-  while (!is_ready()) {
-    // Will do nothing on Arduino but prevent resets of ESP8266 (Watchdog Issue)
-    yield();
-  }
-
-  unsigned long value = 0;
+	unsigned long value = 0;
+  // An array of length 3 with 8 bits each
   uint8_t data[3] = { 0 };
   uint8_t filler = 0x00;
 
-  // pulse the clock pin 24 times to read the data
-  data[2] = shiftIn(DOUT, PD_SCK, MSBFIRST);
-  data[1] = shiftIn(DOUT, PD_SCK, MSBFIRST);
-  data[0] = shiftIn(DOUT, PD_SCK, MSBFIRST);
+  // Giving 8 pulses to the MISO and acquiring 8 bits of data for each pulse
+  // Saving the data to the data array
+	data[2] = shiftIn(DOUT, PD_SCK, MSBFIRST);
+	data[1] = shiftIn(DOUT, PD_SCK, MSBFIRST);
+	data[0] = shiftIn(DOUT, PD_SCK, MSBFIRST);
 
-  // set the channel and the gain factor for the next reading using the clock pin
-  for (unsigned int i = 0; i < GAIN; i++) {
-    digitalWrite(PD_SCK, HIGH);
-    digitalWrite(PD_SCK, LOW);
-  }
+	// Sending additional pulse to the sensor according to the gain value
+	for (unsigned int i = 0; i < GAIN; i++) {
+		digitalWrite(PD_SCK, HIGH);
+		digitalWrite(PD_SCK, LOW);
+	}
 
-  // Replicate the most significant bit to pad out a 32-bit signed integer
-  if (data[2] & 0x80) {
-    filler = 0xFF;
-  } else {
-    filler = 0x00;
-  }
+	// Converting the 3 bytes data to 4 bytes data with the sign information
+  // Using data[2] & 1000 0000 for identifying the sign
+	if (data[2] & 0x80) {
+		filler = 0xFF;
+	}
+	else {
+		filler = 0x00;
+	}
 
-  // Construct a 32-bit signed integer
-  value = ( static_cast<unsigned long>(filler) << 24
-      | static_cast<unsigned long>(data[2]) << 16
-      | static_cast<unsigned long>(data[1]) << 8
-      | static_cast<unsigned long>(data[0]) );
+	// Putting in the identified sign to the first 8 bits and filling in the rest with the data array
+	value = (static_cast<unsigned long>(filler) << 24
+		| static_cast<unsigned long>(data[2]) << 16
+		| static_cast<unsigned long>(data[1]) << 8
+		| static_cast<unsigned long>(data[0]));
 
-  return static_cast<long>(value);
+	return static_cast<long>(value);
 }
 
 long HX711::read_average(byte times) {
-  long sum = 0;
-  for (byte i = 0; i < times; i++) {
-    sum += read();
-    yield();
-  }
-  return sum / times;
+	long sum = 0;
+	for (byte i = 0; i < times; i++) {
+		sum += read();
+		yield();
+	}
+	return sum / times;
 }
 
+// Deleted the - offset due to some occurence yet identified
 double HX711::get_value(byte times) {
-  return read_average(times) - OFFSET;
+	return read_average(times);
 }
 
 float HX711::get_units(byte times) {
-  return get_value(times) / SCALE;
+	return get_value(times) / SCALE;
 }
 
 void HX711::tare(byte times) {
-  double sum = read_average(times);
-  set_offset(sum);
+	double sum = read_average(times);
+	set_offset(sum);
 }
 
 void HX711::set_scale(float scale) {
-  SCALE = scale;
+	SCALE = scale;
 }
 
 float HX711::get_scale() {
-  return SCALE;
+	return SCALE;
 }
 
 void HX711::set_offset(long offset) {
-  OFFSET = offset;
+	OFFSET = offset;
 }
 
 long HX711::get_offset() {
-  return OFFSET;
+	return OFFSET;
 }
 
 void HX711::power_down() {
-  digitalWrite(PD_SCK, LOW);
-  digitalWrite(PD_SCK, HIGH);
+	digitalWrite(PD_SCK, LOW);
+	digitalWrite(PD_SCK, HIGH);
 }
 
 void HX711::power_up() {
-  digitalWrite(PD_SCK, LOW);
+	digitalWrite(PD_SCK, LOW);
 }
