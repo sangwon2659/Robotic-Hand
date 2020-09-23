0cd6ec96a38326df8d0ccbd5dbcbddbcb9de9689
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2020-09-23 10:39:11 +0900

    Update HX711.cpp

--- Arduino/FSS_read_/HX711.cpp
+++ Arduino/FSS_read_/HX711.cpp
@@ -1,53 +1,138 @@
-#ifndef HX711_h
-#define HX711_h
+#include <Arduino.h>
+#include "HX711.h"
 
-#if ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
+#if ARDUINO_VERSION <= 106
+void yield(void) {};
 #endif
 
-class HX711
-{
-  public:
-    byte PD_SCK;  
-    byte DOUT;    
-    byte GAIN;    
-    long OFFSET = 0;  
-    float SCALE = 1;  
-    
-    HX711(byte dout, byte pd_sck, byte gain = 128);
+// Declaration of 객체 and initiating the begin function
+HX711::HX711(byte dout, byte pd_sck, byte gain) {
+  begin(dout, pd_sck, gain);
+}
 
-    HX711();
+HX711::HX711() {
+}
 
-    virtual ~HX711();
-    void begin(byte dout, byte pd_sck, byte gain = 128);
+HX711::~HX711() {
+}
 
-    bool is_ready();
+// Setting pinmodes for each 객체
+void HX711::begin(byte dout, byte pd_sck, byte gain) {
+  PD_SCK = pd_sck;
+  DOUT = dout;
 
-    void set_gain(byte gain = 128);
+  pinMode(PD_SCK, OUTPUT);
+  pinMode(DOUT, INPUT);
 
-    long read();
+  set_gain(gain);
+}
 
-    long read_average(byte times = 1);
-    
-    double get_value(byte times = 1);
+// Original algorithm of identifying whether the sensor is read to send data
+// When the sensor outputs low after sending a packet of data means it is ready to send another
+bool HX711::is_ready() {
+  delayMicroseconds(100);
+  return digitalRead(DOUT) == LOW;
+}
 
-    float get_units(byte times = 1);
+// Gain is about how many additional clocks(MOSI-like) to be sent after receiving 3 bytes of data
+void HX711::set_gain(byte gain) {
+  switch (gain) {
+  case 128:   
+    GAIN = 1;
+    break;
+  case 64:    
+    GAIN = 3;
+    break;
+  case 32:    
+    GAIN = 2;
+    break;
+  }
 
-    void tare(byte times = 10);
+  digitalWrite(PD_SCK, LOW);
+  read();
+}
 
-    void set_scale(float scale = 1.f);
 
-    float get_scale();
+long HX711::read() {
+  unsigned long value = 0;
+  // An array of length 3 with 8 bits each
+  uint8_t data[3] = { 0 };
+  uint8_t filler = 0x00;
 
-    void set_offset(long offset = 0);
+  // Giving 8 pulses to the MISO and acquiring 8 bits of data for each pulse
+  // Saving the data to the data array
+  data[2] = shiftIn(DOUT, PD_SCK, MSBFIRST);
+  data[1] = shiftIn(DOUT, PD_SCK, MSBFIRST);
+  data[0] = shiftIn(DOUT, PD_SCK, MSBFIRST);
 
-    long get_offset();
+  // Sending additional pulse to the sensor according to the gain value
+  for (unsigned int i = 0; i < GAIN; i++) {
+    digitalWrite(PD_SCK, HIGH);
+    digitalWrite(PD_SCK, LOW);
+  }
 
-    void power_down();
+  // Converting the 3 bytes data to 4 bytes data with the sign information
+  // Using data[2] & 1000 0000 for identifying the sign
+  if (data[2] & 0x80) {
+    filler = 0xFF;
+  }
+  else {
+    filler = 0x00;
+  }
 
-    void power_up();
-};
+  // Putting in the identified sign to the first 8 bits and filling in the rest with the data array
+  value = (static_cast<unsigned long>(filler) << 24
+    | static_cast<unsigned long>(data[2]) << 16
+    | static_cast<unsigned long>(data[1]) << 8
+    | static_cast<unsigned long>(data[0]));
 
-#endif /* HX711_h */
+  return static_cast<long>(value);
+}
+
+long HX711::read_average(byte times) {
+  long sum = 0;
+  for (byte i = 0; i < times; i++) {
+    sum += read();
+    yield();
+  }
+  return sum / times;
+}
+
+// Deleted the - offset due to some occurence yet identified
+double HX711::get_value(byte times) {
+  return read_average(times);
+}
+
+float HX711::get_units(byte times) {
+  return get_value(times) / SCALE;
+}
+
+void HX711::tare(byte times) {
+  double sum = read_average(times);
+  set_offset(sum);
+}
+
+void HX711::set_scale(float scale) {
+  SCALE = scale;
+}
+
+float HX711::get_scale() {
+  return SCALE;
+}
+
+void HX711::set_offset(long offset) {
+  OFFSET = offset;
+}
+
+long HX711::get_offset() {
+  return OFFSET;
+}
+
+void HX711::power_down() {
+  digitalWrite(PD_SCK, LOW);
+  digitalWrite(PD_SCK, HIGH);
+}
+
+void HX711::power_up() {
+  digitalWrite(PD_SCK, LOW);
+}
