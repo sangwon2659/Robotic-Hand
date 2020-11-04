c240a12c489fe6c465e83d4d81f2401643e9bbcd
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2020-11-04 11:09:02 +0900

    Add files via upload

---
+++ Research_Touch_Detection/touch_detection.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+import pickle
+from std_msgs.msg import Int32
+from msgpkg.msg import tactile
+
+#Loading the model (filename should be in full)
+filename = '/home/a283/roboticfinger_ros/src/roboticfinger/script/SVM_linear_kernel.sav'
+loaded_model = pickle.load(open(filename,'rb'))
+
+#n_ch refers to the length of data - 1 (the encoder data)
+n_ch = 4
+buffer_ = np.zeros((n_ch,4))
+
+def callback(data):
+	global loaded_model, buffer_
+
+	#Changing the data.Data into an array
+	data = np.asarray(data.Data[0:n_ch])
+	#Addending the newly received data
+	buffer_ = np.append(data,buffer_)
+	
+	#Deleting the oldest dataset in the buffer
+	buffer_ = np.delete(buffer_, (n_ch*4,n_ch*4+1,n_ch*4+2,n_ch*4+3))
+	#Computing the offset
+	offset_ = []
+	for i in range(n_ch): 
+		offset_.append(offset(i))	
+	
+	#Putting in the offset and lastest dataset into the model
+	if buffer_[n_ch-1] != 0:
+		result_ = []
+		for i in range(n_ch):
+			result = touch(offset(i),buffer_[i])
+			result_.append(result)
+		rospy.loginfo(result_)
+
+def talklistener():
+	rospy.init_node('touch', anonymous=True)	
+	rospy.Subscriber("tact", tactile, callback)
+	rospy.spin()
+	#pub = rospy.Publisher('touch', Int32, queue_size=10)
+
+def offset(channel):
+	sum = 0
+	for i in range(1,n_ch):
+		sum += buffer_[channel+n_ch*i]
+	offset = sum/(n_ch-1)
+	return offset
+
+def touch(offset, data):
+	prediction = np.array([[offset,data]])
+	result = loaded_model.predict(prediction)
+	return result
+
+if __name__ == '__main__':
+	try:
+		talklistener()
+	except rospy.ROSInterruptException:
+		pass
+		
+
+
+
+
